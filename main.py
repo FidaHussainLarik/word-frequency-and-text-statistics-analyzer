113c9e6b2fa20169b18dae3dfe01054e7ec1da68
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-07-14 15:36:27 +0500

    feat: add project skeleton with stub functions

---
+++ main.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+DATA_FILE_PATH = Path(__file__).resolve().parent.parent/"sample_data"
+
+# function to take user input (file name) and fetch that specific file and return file-handle back
+def input():
+    # Implement user input and file file-fetching logic here.
+    # Returning a dummy value for now so the rest of the program doesn't crash
+    return None
+
+
+# This function counts words
+def count_words(file):
+    # Will implement word counting logic here.
+    words_count = None
+    return words_count
+
+
+# this will count the total number of sentences in the file
+def count_sentences(file):
+    sent_count = None
+    return sent_count
+
+
+
+# count the number of charaters inside the file,with and without spaces.
+def count_chars(file):
+    char_count = None
+    return char_count
+
+
+# Keep word frequency dictionary/histrogram
+def word_freq(file):
+    histogram = None
+    return histogram
+
+
+# Count top N most frequent words
+def top_most(file):
+    top_10 = None
+    return top_10
+
+# Summary report generator
+def report(file):
+    
+    return None
